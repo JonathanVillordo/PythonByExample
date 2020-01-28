767d83a30b0949100b8ca4c113f7bf59a3c23a24
VillordoMendoza <brain@villorddozasmbp.attlocal.net>
2020-01-28 06:25:06 -0600

    Print the First Name

---
+++ Challenge 001.py
@@ -0,0 +1,9 @@
+#001 Ask for the user’s first name and display the output message Hello [First Name]
+
+def firstName():
+
+        FirstName = input("Enter your First Name: ")
+
+        print("The answer is " + FirstName)
+
+firstName()
