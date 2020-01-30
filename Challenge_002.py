948e94ee61d254d07c1f7fba601471cd0a4b9753
VillordoMendoza <brain@villorddozasmbp.attlocal.net>
2020-01-29 22:22:02 -0600

    Print First Name and Surname

---
+++ Challenge_002.py
@@ -0,0 +1,12 @@
+#002 Ask for the user’s first name and then ask for their surname and display
+#the output message Hello [First Name] [Surname]
+
+def firstSurname():
+
+    FirstName = input("Enter your First Name: ")
+    Surname = input("Enter your Surname: ")
+
+    print ("Hello "+FirstName+' '+Surname)
+
+
+firstSurname()
