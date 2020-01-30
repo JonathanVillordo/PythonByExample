4e70b4703b40464aeaa642fb007be2a90c704797
VillordoMendoza <brain@villorddozasmbp.attlocal.net>
2020-01-30 06:55:40 -0600

    Code in just one line

---
+++ Challenge_003.py
@@ -0,0 +1,4 @@
+#003 Write code that will display the joke “What do you call a bear with no teeth?”
+#and on the next line display the answer “A gummy bear!” Try to create it using only one line of code.
+
+print("What do you call a bear with no teeth?\nA gummy bear")
