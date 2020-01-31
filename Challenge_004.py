ace9af8ff1be586607b5512dad4c99da203d4adc
VillordoMendoza <brain@villorddozasmbp.attlocal.net>
2020-01-31 07:36:40 -0600

    Add two numbers

---
+++ Challenge_004.py
@@ -0,0 +1,11 @@
+#004 Ask the user to enter two numbers.
+#Add them together and display the answer as The total is [answer]
+
+def add():
+    a = int(input("Enter a number :"))
+    b = int(input("Enter a number :"))
+
+
+    print(a+b)
+
+add()
