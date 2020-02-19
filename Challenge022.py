b4c9f1ebf67b79e8c99bd59bcc559d9be81064a0
VillordoMendoza <brain@villorddozasmbp.attlocal.net>
2020-02-19 06:21:45 -0600

    Title function

---
+++ Challenge022.py
@@ -0,0 +1,11 @@
+# 022 Ask the user to enter their first name and surname in lower case.
+# Change the case to title case and join them together. Display the finished result.
+def titlename():
+
+    name = input("Enter your Name and Surname in lower case: ")
+
+
+    print('{0}'.format(name.title()))
+
+
+titlename()
