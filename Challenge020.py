a59158b6e62921bab0ed1bd73f48b299f706fea9
VillordoMendoza <brain@villorddozasmbp.attlocal.net>
2020-02-19 06:06:30 -0600

    Lenght Name

---
+++ Challenge020.py
@@ -0,0 +1,8 @@
+# 020 Ask the user to enter their first name and then display the length of their name.
+def lengname():
+    name = input("Enter your First Name: ")
+
+    print ('Your Length Name is {0}'.format(len(name)))
+
+
+lengname()
