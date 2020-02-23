e4ee814c32c369647ae6ed61ec021a689f1f91d5
VillordoMendoza <brain@villorddozasmbp.attlocal.net>
2020-02-23 09:45:28 -0600

    Multiply numbers

---
+++ Challenge_027.py
@@ -0,0 +1,11 @@
+# 027 Ask the user to enter a number with lots of decimal places. Multiply this number by two and display the answer.
+def multiply():
+
+    number = float(input("Enter a number: "))
+
+    print (number * 2)
+
+
+
+
+multiply()
