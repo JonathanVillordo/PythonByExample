eeaf49586550f1a0bda43021189d584100703905
VillordoMendoza <brain@villorddozasmbp.attlocal.net>
2020-02-24 19:48:12 -0600

    Multiply and round

---
+++ Challenge_028.py
@@ -0,0 +1,9 @@
+# 028 Update program 027 so that it will display the answer to two decimal places.
+
+def multiplyby2():
+
+    number = float(input("Enter a number whit bunch of Decimals: "))
+
+    print (round(number*2,2))
+
+multiplyby2()
