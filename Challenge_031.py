e9fcd5e65e30565dec23c53e9ca0c8649fcafc8e
VillordoMendoza <brain@villorddozasmbp.attlocal.net>
2020-02-27 22:04:24 -0600

    Circle radius

---
+++ Challenge_031.py
@@ -0,0 +1,11 @@
+# 031 Ask the user to enter the radius of a circle
+#(measurement from the centre point to the edge). Work out the area of the circle (π*radius2).
+import math
+def circle():
+
+    radius = int(input("Enter the radius of a circle: "))
+
+    print(math.pi*radius**2)
+
+circle()
+ 
